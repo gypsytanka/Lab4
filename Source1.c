744a32ae02374638f6afdd5c5c769e8daa45e02e
Татьяна <tanya.maslova.0707@mail.ru>
2025-10-04 15:16:30 +0300

    Add files via upload

---
+++ Source1.c
@@ -0,0 +1,30 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
+
+int main() {
+
+    setlocale(LC_CTYPE, "RUS");
+
+    int codeA, codeB;
+
+    printf("Введите код сканера A: ");
+    scanf("%d", &codeA);
+
+    printf("Введите код сканера B: ");
+    scanf("%d", &codeB);
+
+    int a = (codeA % 2 == 0);
+    int b = (codeB % 2 == 0);
+
+    int door = (a && !b) || (!a && b);
+
+    if (door) {
+        printf("Дверь открыта!\n");
+    }
+    else {
+        printf("Дверь закрыта.\n");
+    }
+
+    return 0;
+}
